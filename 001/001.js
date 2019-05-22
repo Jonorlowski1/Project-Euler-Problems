d70f19e33f41a252b034a5206a7060c11d1d8cf0
Jonathan Orlowski <jon.orlowski1@gmail.com>
2019-05-22 14:45:21 -0500

    updated 001 to JS file

--- 001/001.js
+++ 001/001.js
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Solution #001</title>
-
-</head>
-
-<body>
-  <script src="https://code.jquery.com/jquery.js"></script>
-  <script type="text/javascript">
-
     // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
     // Find the sum of all the multiples of 3 or 5 below 1000.
@@ -29,10 +14,3 @@
     }
   
     sumNumbers();
-
-    
-  </script>
-
-</body>
-
-</html>
